1abe198f19adac6d9b572a291df78de3fbfec52e
IvanRulevskyi <rulevskiy4@gmail.com>
2024-04-11 15:57:04 +0300

    newFile

---
+++ Initializers, tatic changes, this, copying/Function.cpp
@@ -0,0 +1,8 @@
+//
+//  Function.cpp
+//  Initializers, tatic changes, this, copying
+//
+//  Created by Иван Рулевский on 11.04.2024.
+//
+
+#include "Function.hpp"
---
+++ Initializers, tatic changes, this, copying/Function.hpp
@@ -0,0 +1,13 @@
+//
+//  Function.hpp
+//  Initializers, tatic changes, this, copying
+//
+//  Created by Иван Рулевский on 11.04.2024.
+//
+
+#ifndef Function_hpp
+#define Function_hpp
+
+#include <stdio.h>
+
+#endif /* Function_hpp */
